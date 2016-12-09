58d252d870f6829a100b51b94e2f6a5d9122c443
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2016-12-09 17:14:39 -0200

    Usando pool de threads.

--- src/br/com/alura/servidor/ServidorTarefas.java
+++ src/br/com/alura/servidor/ServidorTarefas.java
@@ -3,10 +3,14 @@ package br.com.alura.servidor;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorTarefas {
 
 	public static void main(String[] args) throws IOException {
+	
+		ExecutorService threadPool = Executors.newCachedThreadPool();
 		
 		System.out.println("---- Iniciando Servidor -----");
 		ServerSocket serverSocket = new ServerSocket(12345);
@@ -15,7 +19,7 @@ public class ServidorTarefas {
 			Socket socket = serverSocket.accept();
 			
 			System.out.println("Aceitando novo cliente na porta " + socket.getPort());
-			new Thread(new DistribuirTarefas(socket)).start();
+			threadPool.execute(new DistribuirTarefas(socket));
 		}
 	}
 }
