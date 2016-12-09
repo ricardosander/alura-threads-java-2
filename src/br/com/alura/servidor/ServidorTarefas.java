da20c5e8533368aa39f8bed8da3b68a2ef6a1cfd
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2016-12-09 15:21:43 -0200

    Criando classe ServidorTarefas

---
+++ src/br/com/alura/servidor/ServidorTarefas.java
@@ -0,0 +1,18 @@
+package br.com.alura.servidor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTarefas {
+
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("---- Iniciando Servidor -----");
+		ServerSocket serverSocket = new ServerSocket(12345);
+		while(true) {
+			Socket socket = serverSocket.accept();
+			System.out.println("Aceitando novo cliente na porta " + socket.getPort());
+		}
+	}
+}
